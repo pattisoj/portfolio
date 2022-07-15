5ca51bdc58182f2c17d6b7c9e0abd780baa2577f
pattisoj <joshpattison23@icloud.com>
2022-07-15 23:35:03 +0100

    Updated PageNumber positioning

--- components/PageNumber.js
+++ components/PageNumber.js
@@ -1,7 +1,7 @@
 export default function PageNumber({ pageNumber }) {
   const numberDisplay = `${pageNumber} - 4`;
   return (
-    <p className="w-16 self-center rotate-90 ml-80 mt-10 mr-36 sm:mr-0 tracking-tighter opacity-50 text-base font-Graduate">
+    <p className="absolute w-16 self-center rotate-90 ml-80 mt-[29rem] mr-36 sm:mr-0 tracking-tighter opacity-50 text-base font-Graduate">
       {numberDisplay}
     </p>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col w-screen  h-screen bg-cover bg-right sm:bg-center bg-[url('../images/home-bg.jpg')]">
       <Navbar activePage={"home"} />
+      <PageNumber pageNumber={1} />
       <div className="ml-4 md:ml-44 mt-12 self-top">
         <p className="font-Abril text-base sm:text-lg tracking-wide mb-2">
           hello, my name is
@@ -15,10 +16,10 @@ export default function Home() {
         <p className="font-Abril text-1xl sm:text-3xl tracking-wide mb-4">
           I enjoy coding things that solve problems
         </p>
-        <p className="font-Abril text-xs sm:text-base tracking-wide">
+        <p className="font-Abril text-xs sm:text-sm tracking-wide">
           I love developing and producing high quality software.
         </p>
-        <p className="font-Abril text-xs sm:text-base tracking-wide mb-6">
+        <p className="font-Abril text-xs sm:text-sm tracking-wide mb-6">
           I am always looking to learn new things and I am currently studying
           software development at the School of Code
         </p>
@@ -44,7 +45,6 @@ export default function Home() {
           </a>
         </span>
       </div>
-      <PageNumber pageNumber={1} />
     </div>
   );
 }
