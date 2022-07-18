4e4a7b79acaf645b60ad2ece95b8d6c946a13ef2
pattisoj <joshpattison23@icloud.com>
2022-07-18 15:01:07 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Graduate&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Graduate&display=swap&family=Roboto:wght@300&display=swap"
             rel="stylesheet"
           />
         </Head>
