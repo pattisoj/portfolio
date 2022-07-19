aa8373c636b9c8a7c6bff2608eff8a7693e6a92e
pattisoj <joshpattison23@icloud.com>
2022-07-19 12:09:29 +0100

    Cypress Navigation tests

---
+++ cypress/e2e/navigation.cy.js
@@ -0,0 +1,40 @@
+describe("Navigation", () => {
+  it("should navigate to the about page", () => {
+    cy.visit("http://localhost:3000/");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="about"]').click();
+
+    cy.url().should("include", "/about");
+
+    cy.get("h1").should("have.text", "The About Page is a work in progress...");
+  });
+
+  it("should navigate to the projects page", () => {
+    cy.visit("http://localhost:3000/");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="projects"]').click();
+
+    cy.url().should("include", "/projects");
+
+    cy.get("h1").should(
+      "have.text",
+      "The Projects Page is a work in progress..."
+    );
+  });
+
+  it("should navigate to the contact page", () => {
+    cy.visit("http://localhost:3000/");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="contact"]').click();
+
+    cy.url().should("include", "/contact");
+
+    cy.get("h1").should(
+      "have.text",
+      "The Contact Page is a work in progress..."
+    );
+  });
+});
