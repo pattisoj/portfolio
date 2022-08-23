cc34c3a6aa39f323f9fd5456305658860acd6c7a
pattisoj <joshpattison23@icloud.com>
2022-08-23 12:30:58 +0100

    Updated cursor tracker for contact page

--- components/CursorTracker.js
+++ components/CursorTracker.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import Totoro from "../images/contact/totoro-line-art.png";
 
-export default function CursorTracker() {
+export default function CursorTracker({ id }) {
   const [dateTime, setDateTime] = useState('"DATE" - "TIME"');
   const [cursorTrackerDisplay, setCursorTrackerDisplay] = useState("0/0");
 
@@ -21,12 +23,26 @@ export default function CursorTracker() {
   }
 
   return (
-    <div className="font-Graduate absolute text-right self-end sm:mt-[32rem] md:mt-[22rem] opacity-0 lg:opacity-50 text-sm mr-10 z-10">
+    <div
+      className="font-Graduate absolute text-right self-end sm:mt-[32rem] md:mt-[22rem] opacity-0 lg:opacity-50 text-sm mr-10 z-10"
+      id={id}
+    >
+      {id === "contact" ? (
+        <Image
+          alt="Totoro"
+          src={Totoro}
+          width="100%"
+          height="100%"
+          layout="responsive"
+          objectFit="contain"
+        />
+      ) : null}
       <a href="mailto:joshpattison23@icloud.com">
         <p className="hover:underline">joshpattison23@icloud.com</p>
       </a>
       <p>{dateTime}</p>
       <p>{cursorTrackerDisplay}</p>
+      {id === "contact" ? <p className="mt-2"> 4 - 4 </p> : null}
     </div>
   );
 }
