9036e3d5053052b12c9a94473cc80d4814c34695
pattisoj <joshpattison23@icloud.com>
2022-08-23 12:49:16 +0100

    Tested contact page

---
+++ cypress/e2e/contact.cy.js
@@ -0,0 +1,72 @@
+describe("Contact Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/contact");
+  });
+
+  it("should display correct text content", () => {
+    cy.get(".contact-title").should("have.text", "Where to find me");
+
+    cy.get(".contact-links-container > :nth-child(1)").should(
+      "have.text",
+      "You can email me here - joshpattison23@icloud.com"
+    );
+
+    cy.get(".contact-links-container > :nth-child(2)").should(
+      "have.text",
+      "GitHub followers are the most valuable - github.com/pattisoj"
+    );
+
+    cy.get(".contact-links-container > :nth-child(3)").should(
+      "have.text",
+      "Oh, we should definitely connect - linkedin.com/in/josh-pattison"
+    );
+
+    cy.get(".contact-links-container > :nth-child(4)").should(
+      "have.text",
+      "Keep up with my day-to-day on Twitter - twitter.com/_pattisoj"
+    );
+
+    cy.get(".contact-links-container > :nth-child(5)").should(
+      "have.text",
+      "Once in a while I blog - pattisoj.hashnode.dev"
+    );
+  });
+
+  it("should display correct image", () => {
+    cy.get(
+      '.contact-totoro-container > [style="box-sizing:border-box;display:block;overflow:hidden;width:initial;height:initial;background:none;opacity:1;border:0;margin:0;padding:0;position:relative"] > img'
+    ).should("be.visible");
+  });
+
+  it("should have correct hyper links", () => {
+    cy.get(".contact-links-container > :nth-child(1) > a").should(
+      "have.attr",
+      "href",
+      "mailto:joshpattison23@icloud.com"
+    );
+
+    cy.get(".contact-links-container > :nth-child(2) > a").should(
+      "have.attr",
+      "href",
+      "https://github.com/pattisoj"
+    );
+
+    cy.get(".contact-links-container > :nth-child(3) > a").should(
+      "have.attr",
+      "href",
+      "https://www.linkedin.com/in/josh-pattison/"
+    );
+
+    cy.get(".contact-links-container > :nth-child(4) > a").should(
+      "have.attr",
+      "href",
+      "https://twitter.com/_pattisoj"
+    );
+
+    cy.get(".contact-links-container > :nth-child(5) > a").should(
+      "have.attr",
+      "href",
+      "https://pattisoj.hashnode.dev/"
+    );
+  });
+});
