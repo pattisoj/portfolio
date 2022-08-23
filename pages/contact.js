af435305c06a54189370401d0d4927616d1bd91c
pattisoj <joshpattison23@icloud.com>
2022-08-23 12:30:15 +0100

    Added contact page content

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
+import Image from "next/image.js";
+import Ponyo from "../images/contact/ponyo.png";
 import NavBar from "../components/NavBar.js";
 import CursorTracker from "../components/CursorTracker.js";
 import PageNumber from "../components/PageNumber";
-import LogoBar from "../components/LogoBar.js";
 
 export default function Contact() {
   return (
@@ -12,9 +13,74 @@ export default function Contact() {
       </Head>
       <div className="contact-page-container">
         <NavBar activePage={"contact"} />
-        <PageNumber pageNumber={4} />
-        <LogoBar />
-        <CursorTracker />
+        <CursorTracker id={"contact"} />
+        <div className="contact-information-container">
+          <h2 className="contact-title">Where to find me</h2>
+          <div className="contact-contents-container">
+            <div className="contact-links-container">
+              <p className="contact-p">
+                You can email me here -{" "}
+                <a
+                  href="mailto:joshpattison23@icloud.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  joshpattison23@icloud.com
+                </a>
+              </p>
+              <p className="contact-p">
+                GitHub followers are the most valuable -{" "}
+                <a
+                  href="https://github.com/pattisoj"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  github.com/pattisoj
+                </a>
+              </p>
+              <p className="contact-p">
+                Oh, we should definitely connect -{" "}
+                <a
+                  href="https://www.linkedin.com/in/josh-pattison/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  linkedin.com/in/josh-pattison
+                </a>
+              </p>
+              <p className="contact-p">
+                Keep up with my day-to-day on Twitter -{" "}
+                <a
+                  href="https://twitter.com/_pattisoj"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  twitter.com/_pattisoj
+                </a>
+              </p>
+              <p className="contact-p">
+                Once in a while I blog -{" "}
+                <a
+                  href="https://pattisoj.hashnode.dev/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  pattisoj.hashnode.dev
+                </a>
+              </p>
+            </div>
+            <div className="contact-totoro-container">
+              <Image
+                alt="Ponyo"
+                src={Ponyo}
+                width="100%"
+                height="100%"
+                layout="responsive"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
