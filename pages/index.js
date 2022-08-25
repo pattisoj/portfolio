e4910b03e2bf29e56a2047e82c12b77d985469c3
pattisoj <joshpattison23@icloud.com>
2022-08-25 16:35:21 +0100

    Changed homepage content

--- pages/index.js
+++ pages/index.js
@@ -10,95 +10,48 @@ export default function Home() {
       <Head>
         <title>Josh Pattison</title>
       </Head>
-      <div className="flex flex-col w-screen h-screen bg-cover bg-right sm:bg-center bg-[url('../images/home-bg.jpg')]">
+      <div className="home-page-container">
         <NavBar activePage={"home"} />
         <PageNumber pageNumber={1} />
         <LogoBar />
         <CursorTracker />
-        <div className="ml-4 lg:ml-44 mt-12 self-top">
-          <p
-            id="greeting"
-            className="font-Roboto text-base md:text-lg tracking-wide mb-2"
-          >
-            hello, my name is
-          </p>
-          <h1
-            id="titleName"
-            className="font-Abril text-4xl md:text-6xl tracking-wide mb-2"
-          >
-            Josh Pattison
-          </h1>
-          <p
-            id="introLineOne"
-            className="font-Abril text-1xl md:text-3xl tracking-wide mb-4"
-          >
-            I enjoy coding things that solve problems
-          </p>
-          <p
-            id="introLineTwo"
-            className="font-Roboto text-xs md:text-sm tracking-wide"
-          >
-            I love developing and producing high quality software.
-          </p>
-          <p
-            id="introLineThree"
-            className="font-Roboto text-xs md:text-sm tracking-wide mb-6"
-          >
-            I am always looking to learn and I am currently studying software
-            development at the School of Code
-          </p>
-          <p
-            id="websiteWelcome"
-            className="font-Abril text-sm md:text-lg tracking-wide mb-4"
-          >
-            welcome to my website.
-          </p>
-          <span>
-            <a
-              id="linkedInATag"
-              href="https://www.linkedin.com/in/josh-pattison/"
-              target="_blank"
-              rel="noreferrer"
-              className="font-Graduate text-sm md:text-base tracking-wide text-red-600"
-            >
-              <button
-                id="linkedInCallToAction"
-                type="button"
-                className="text-red-600 border border-gray-800 opacity-80 hover:bg-gray-200 hover:bg-opacity-40 hover:shadow-xl hover:opacity-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg px-2 py-1 mr-2"
+        <div className="home-content-container">
+          <p>hello, my name is</p>
+          <h2 className="home-title">Josh Pattison</h2>
+          <div className="home-contents-container">
+            <div className="home-information-container">
+              <p>
+                I enjoy creating beautiful webpages and coding things to solve
+                problems.
+              </p>
+              <p className="home-p">
+                I achieved a BSc in Psychology and recently graduated from the
+                School of Code 16 week bootcamp. I pride myself in developing
+                well tested and high quality software. I am always looking to
+                learn new things so I am currently learning Python!
+              </p>
+              <br />
+              <p>Welcome to my website.</p>
+              <br />
+            </div>
+            <div className="home-button-container">
+              <br />
+              <a
+                href="https://www.linkedin.com/in/josh-pattison/"
+                target="_blank"
+                rel="noreferrer"
               >
-                linkedin
-              </button>
-            </a>
-            <a
-              id="gitHubATag"
-              href="https://github.com/pattisoj"
-              target="_blank"
-              rel="noreferrer"
-              className="font-Graduate text-sm md:text-base tracking-wide text-red-600"
-            >
-              <button
-                id="gitHubCallToAction"
-                type="button"
-                className="text-red-600 border border-gray-800 opacity-80 hover:bg-gray-200 hover:bg-opacity-40 hover:shadow-xl hover:opacity-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg px-2 py-1 mr-2"
+                LinkedIn
+              </a>
+              <a
+                href="https://github.com/pattisoj"
+                target="_blank"
+                rel="noreferrer"
               >
-                github
-              </button>
-            </a>
-            <a
-              href="/josh-pattison-cv.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-Graduate text-sm sm:text-base tracking-wide opacity-70 hover:opacity-100 self-center mr-2"
-            >
-              <button
-                id="resumeCallToAction"
-                type="button"
-                className="visible sm:invisible text-red-600 border border-gray-800 opacity-80 hover:bg-gray-200 hover:bg-opacity-40 hover:shadow-xl hover:opacity-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg px-2 py-1"
-              >
-                resume
-              </button>
-            </a>
-          </span>
+                GitHub
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
