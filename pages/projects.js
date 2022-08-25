7ec81465ff1ca4818e1383e48bc222fa3d919890
pattisoj <joshpattison23@icloud.com>
2022-08-25 14:32:52 +0100

    Added project content

--- pages/projects.js
+++ pages/projects.js
@@ -43,7 +43,7 @@ export default function Projects() {
               }`}
               onClick={() => handleClick("2")}
             >
-              2.
+              Npm package.
             </button>
             <button
               className={`about-option-button ${
@@ -51,7 +51,7 @@ export default function Projects() {
               }`}
               onClick={() => handleClick("3")}
             >
-              3.
+              Travel application.
             </button>
             <button
               className={`about-option-button ${
@@ -59,7 +59,7 @@ export default function Projects() {
               }`}
               onClick={() => handleClick("4")}
             >
-              4.
+              Revision application.
             </button>
             <button
               className={`about-option-button ${
@@ -67,7 +67,7 @@ export default function Projects() {
               }`}
               onClick={() => handleClick("5")}
             >
-              5.
+              ...
             </button>
           </div>
           <div className="projects-info-display">
@@ -85,32 +85,236 @@ export default function Projects() {
             {displayedOption === "2" ? (
               <div className="projects-info-content-container">
                 <fieldset className="projects-info-content">
-                  <legend>PROJECT NAME</legend>
-                  <p>placeholder text 2</p>
+                  <legend>JPSTRING</legend>
+                  <div className="projects-buttons">
+                    <a
+                      href="https://github.com/pattisoj/jpString"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Project repository.
+                    </a>
+                    <br />
+                    <p>-</p>
+                    <br />
+                    <a
+                      href="https://www.npmjs.com/package/jp-string"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      View on npm.
+                    </a>
+                    <br />
+                    <p>-</p>
+                    <br />
+                    <p className="languages">JavaScript. Jest.</p>
+                  </div>
+                  <br />
+                  <p>An NPM string manipulation package.</p>
+                  <br />
+                  <mark>
+                    This is an open source project that anyone can contribute
+                    to.
+                  </mark>
+                  <br />
+                  <br />
+                  <p>
+                    <ins>
+                      I created this NPM package as a learning opportunity.
+                    </ins>
+                    <br />I wanted to experience and learn about the creation of
+                    a public package.
+                    <br /> I also wanted to{" "}
+                    <mark>learn about CI/CD pipelines</mark> and this project
+                    was my first experience of it. I was able to create a GitHub
+                    workflow that runs all tests on a push or pull request and
+                    if they pass is able to automatically publish to npm if the
+                    version is updated! This facilitated the addition of a CI/CD
+                    pipeline to my final project where I additionally
+                    experimented with running ESLint on a pull request!
+                  </p>
+                  <br />
+                  <p>
+                    I chose a string manipulation package as I felt it gave me
+                    the most opportunities to keep adding functionality. I wrote
+                    all of the methods and tests in the library and all of the
+                    documentation. <br />
+                    <mark>This library is fully tested (100% coverage).</mark>
+                  </p>
                 </fieldset>
               </div>
             ) : null}
             {displayedOption === "3" ? (
               <div className="projects-info-content-container">
                 <fieldset className="projects-info-content">
-                  <legend>PROJECT NAME</legend>
-                  <p>placeholder text 3</p>
+                  <legend>FINDERS KEEPERS TRAVEL APPLICATION</legend>
+                  <div className="projects-buttons">
+                    <a
+                      href="https://github.com/pattisoj/final-project-travel-app-team-jenga"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Project repository.
+                    </a>
+                    <br />
+                    <p>-</p>
+                    <br />
+                    <a
+                      href="https://finders-keepers-soc.netlify.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live site.
+                    </a>
+                    <br />
+                    <p>-</p>
+                    <br />
+                    <p className="languages">
+                      HTML. CSS. JavaScript. AWS. Jest. Cypress. Auth0.
+                    </p>
+                  </div>
+                  <br />
+                  <p>
+                    A Travel app for any user.
+                    <mark>Created as a team of 5 in 4 weeks.</mark> (Week 13 -
+                    16 of our 16 week bootcamp.)
+                  </p>
+                  <br />
+                  <mark>The Brief</mark>
+                  <p>
+                    The brief for this project was <ins>brief</ins> (no pun
+                    intended!). We had to simply identify and solve a problem
+                    using technology.
+                  </p>
+                  <br />
+                  <mark>The Problems & Our Solution</mark>
+                  <p>
+                    So what problems did we find? The problem this app aimed to
+                    solve can be split into two sections:{" "}
+                  </p>
+                  <br />
+                  <ins>Too much information</ins>
+                  <p>
+                    During our consumer research, before app creation, we found
+                    that consumers were overwhelmed by all of the information
+                    out there when they were trying to book a holiday. They had
+                    way too many tabs open and felt they had to hold a lot of
+                    information between webpages to inform their decision We set
+                    out to solve this by creating an app that could store all
+                    the information you would need in one place. This is the
+                    first purpose of FINDERS KEEPERS.
+                  </p>
+                  <br />
+                  <ins> Profit influenced information</ins>
+                  <p>
+                    Whilst doing our research to source the best information for
+                    our new application we discovered a second problem; profit
+                    influenced the information websites provided. We found that
+                    websites seemed to avoid tougher topics like crime or
+                    possible negatives to travel in order to sell you their
+                    travel guides for example.
+                  </p>
+                  <br />
+                  <p>
+                    {`
+                    This gave us a unique opportunity. We didn't need to make
+                    profit. We decided to take this as a bonus and focus on showing the
+                    truthful information - unedited and unbiased. To achieve
+                    this we didn't cherry pick our information and decided to
+                    display it to users as is. Giving our users full confidence
+                    in our information.`}
+                  </p>
                 </fieldset>
               </div>
             ) : null}
             {displayedOption === "4" ? (
               <div className="projects-info-content-container">
                 <fieldset className="projects-info-content">
-                  <legend>PROJECT NAME</legend>
-                  <p>placeholder text 4</p>
+                  <legend>SOC REVISION APPLICATION</legend>
+                  <div className="projects-buttons">
+                    <a
+                      href="https://github.com/pattisoj/soc-revision-app"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Project repository.
+                    </a>
+                    <br />
+                    <p>-</p>
+                    <br />
+                    <a
+                      href="https://team16.netlify.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live site.
+                    </a>
+                    <br />
+                    <p>-</p>
+                    <br />
+                    <p className="languages">
+                      HTML. CSS. JavaScript. SQL. Jest. Cypress.
+                    </p>
+                  </div>
+                  <br />
+                  <p>
+                    A Revision app for the School of Code.{" "}
+                    <mark>Created as a team of 4 in one week</mark> (5 days).
+                  </p>
+                  <br />
+                  <p>
+                    This project was completed for week 9 of our 16 week
+                    bootcamp. I personally only had{" "}
+                    <ins>8 weeks of coding experience</ins> going into this
+                    project!
+                  </p>
+                  <br />
+                  <p>
+                    <mark>The Brief</mark>
+                  </p>
+                  <p>
+                    {` We had to use what we had learnt on
+                    the course up until that point to try and improve the lives
+                    of our fellow bootcampers. We had to consider our user's
+                    experiences and their problems. Specifically, we wanted to
+                    enhance the remote experience of a bootcamper, or find
+                    something that could help them with the vast amount of
+                    learning they're undertaking.`}
+                  </p>
+                  <br />
+                  <p>
+                    <mark>The Problem & Our Solution</mark>{" "}
+                  </p>
+                  <p>
+                    {`After sending out a
+                    survey to our fellow bootcampers we found what they wanted
+                    most was more time. As we couldn't code more hours into the
+                    day we decided to try and facilitate the best use of their
+                    time outside of bootcamp hours by creating a revision app.`}
+                  </p>
                 </fieldset>
               </div>
             ) : null}
             {displayedOption === "5" ? (
               <div className="projects-info-content-container">
                 <fieldset className="projects-info-content">
-                  <legend>PROJECT NAME</legend>
-                  <p>placeholder text 5</p>
+                  <legend>COMING SOON</legend>
+                  <p>This space is empty for now...</p>
+                  <p>
+                    I am looking forward to sharing what I have been working on.
+                  </p>
+                  <br />
+                  <p>
+                    For a sneak peak of my work <mark> right now</mark> be sure
+                    to head over to my{" "}
+                    <a
+                      href="https://github.com/pattisoj"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <ins> GitHub.</ins>
+                    </a>
+                  </p>
                 </fieldset>
               </div>
             ) : null}
@@ -123,18 +327,11 @@ export default function Projects() {
         </fieldset>
         <div className="about-footer">
           <blockquote className="about-blockquote">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-            ullamcorper ut nibh nec sollicitudin. Aliquam erat volutpat. Donec
-            laoreet arcu diam, sed scelerisque lectus venenatis et. Nam lacinia
-            nisi eget dui hendrerit placerat. Nullam at pellentesque massa,
-            elementum porttitor nisl. Donec iaculis risus vel mauris elementum
-            ultrices. Duis ac neque lectus. Mauris commodo dolor tristique arcu
-            vehicula, non venenatis risus blandit. Aenean at gravida ipsum, non
-            gravida enim.
+            {`After completing the School of Code I have continued my learning on
+            freeCodeCamp. I am currently working to my first certification
+            "Scientific Computing with Python".  I am also working on some projects with other bootcampers. To keep up to date with the progress of these projects head over to my github.`}
             <cite className="about-cite">
-              <a href="https://en.wikipedia.org/wiki/Nier:_Automata">
-                Wikipedia
-              </a>
+              <a href="https://en.wikipedia.org/wiki/Nier:_Automata">GitHub</a>
             </cite>
           </blockquote>
         </div>
