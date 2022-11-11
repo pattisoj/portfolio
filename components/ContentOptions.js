e969202c6a4e3d9ebc591e1c22b583e0a0033fa2
pattisoj <joshpattison23@icloud.com>
2022-11-11 15:39:38 +0000

    Setup ContentOptions map

--- components/ContentOptions.js
+++ components/ContentOptions.js
@@ -1,8 +1,11 @@
+import data from "../data/projectsData.json";
+
 export default function ContentOptions({
   displayedOption,
   setDisplayedOption,
 }) {
   function handleClick(option) {
+    console.log(option);
     setDisplayedOption(option);
   }
 
@@ -10,12 +13,24 @@ export default function ContentOptions({
     <div className="projects-options">
       <button
         className={`about-option-button ${
-          "1" === displayedOption ? "active" : ""
+          1 === displayedOption ? "active" : ""
         }`}
-        onClick={() => handleClick("1")}
+        onClick={() => handleClick(1)}
       >
         Welcome.
       </button>
+
+      {data.map((option, index) => (
+        <button
+          key={index}
+          className={`about-option-button ${
+            index + 2 === displayedOption ? "active" : ""
+          }`}
+          onClick={() => handleClick(index + 2)}
+        >
+          {option.name}
+        </button>
+      ))}
     </div>
   );
 }
